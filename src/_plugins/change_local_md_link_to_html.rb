6f113776598ac6e99bb34b0c52788ce5e4d6953b
Shipow <shipowlata@gmail.com>
2015-06-21 15:39:16 +0200

    fix prod config

--- src/_plugins/change_local_md_link_to_html.rb
+++ src/_plugins/change_local_md_link_to_html.rb
@@ -1,11 +1,13 @@
 module ChangeLocalMdLinksToHtml
   class Generator < Jekyll::Generator
+    def initialize(base)
+      @base_dir  = base
+    end
     def generate(site)
-      @base_dir = Jekyll.configuration({})['url']
       site.posts.each { |p| rewrite_links(site, p) }
     end
     def rewrite_links(site, post)
-      post.content = post.content.gsub(/\(([^)]+)\.md\)/){"(#{@base_dir}/docs/#{$1.downcase.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-')}/)"}
+      post.content = post.content.gsub(/\(([^)]+)\.md\)/){"(#{@base_dir}docs/#{$1.downcase.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-')}/)"}
     end
   end
 end
