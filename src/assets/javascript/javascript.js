b88e2b335f20830a0fc240c24b3765d749393e0b
Shipow <shipowlata@gmail.com>
2015-11-14 22:08:49 +0100

    update search

--- src/assets/javascript/javascript.js
+++ src/assets/javascript/javascript.js
@@ -1,31 +1,53 @@
+'use strict';
 // ----------------------------------------------
 // Common application
 // ----------------------------------------------
-$(function () {
-  var $q = $('#search');
+/* global $ */
 
+$(function () {
   // hack to force autocomplete='off' on Chrome
   var hackTimeout = setTimeout(function() {
-    $q.attr('autocomplete', 'off');
+    $('#search').attr('autocomplete', 'off');
     clearTimeout(hackTimeout);
   }, 0);
 
-  // typeahead.js initialization
-  $('#search').typeahead({hint: false}, {
-    source: index.ttAdapter({hitsPerPage: 5}),
-    displayKey: 'title',
-    templates: {
-      suggestion: function(hit) {
-        // render the hit
-        return '<div class="hit">' +
-          '<div class="name">' +
-            hit._highlightResult.title.value +
+  autocomplete('#search', {
+    hint: false,
+    debug: true
+  },
+  [
+    {
+      source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+      displayKey: 'title',
+      templates: {
+        footer: '<div class="branding">Powered by <img src="https://www.algolia.com/assets/algolia128x40.png" /></div>',
+        suggestion: function(suggestion) {
+
+          var template = '<div class="hit"><div class="row">' +
+          '<div class="col-xs-4">' +
+          '<span class="title">' +
+            suggestion._highlightResult.title.value +
+          '</span>' +
           '</div>' +
-          '<div class="snippet">[...]'+  hit._snippetResult.content.value + '[...]</div>' +
-        '</div>';
+          '<div class="col-xs-8">'+
+            '<div class="path">';
+
+          if (typeof suggestion.h2 !== 'undefined'){ template = template.concat('<span class="path">' +  suggestion._highlightResult.h2.value + '</span>') };
+
+          if (typeof suggestion.h3 !== 'undefined'){ template = template.concat('<span class="path"> > ' +  suggestion._highlightResult.h3.value + '</span>') };
+
+          template = template.concat(
+            '</div>' +
+            '<div class="snippet">' +  suggestion._highlightResult.text.value + '</div>' +
+          '</div>' +
+        '</div></div>');
+
+        return template;
+        }
       }
     }
-  }).on('typeahead:selected', function(event, suggestion, dataset) {
+  ]).on('autocomplete:selected', function(event, suggestion, dataset) {
     location.href = window.baseUrl + suggestion.url;
-  }).focus;;
+  }).focus();
+
 });
